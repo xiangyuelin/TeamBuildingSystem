22784df8029833f20d3f2af89f7e3b4b93502d8e
xiangyuelin <linjiaxiangyue@gmail.com>
2014-10-27 16:00:14 +0800

    add models

---
+++ models/model.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+from extension import db
+
+class Admin(db.Model):
+    __tablename__ = "admin"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(20))
+
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(20))
+    email = db.Column(db.String(30))
+    phone = db.Column(db.String(20))
+
+
+class FruitAdmin(db.Model):
+    __tablename__ = "FruitAdmin"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(20))
+
+
+class Activity(db.Model):
+    __tablename__ = "activities"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    type = db.Column(db.Integer, default=1)
+    show = db.Column(db.Boolean, default=True)
+    remark = db.Column(db.Text)
+    #remind = db.Column(db.Boolean, default=False)
+    validate_period = db.Column(db.Integer, default=1)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Item(db.Model):
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    activity_id = db.Column(db.Integer)
+
+
+class Vote(db.Model):
+    __tablename__ = "votes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    item_id = db.Column(db.Integer)
+
+#comment for the activities not for items
+class Comment(db.Model):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    activity_id = db.Column(db.Integer)
+    content = db.Column(db.String(20)), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+
+
+
+
