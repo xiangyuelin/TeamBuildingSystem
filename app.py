1f25054a123723f6c8b5a06fb45795f14f733f38
xiangyuelin <linjiaxiangyue@gmail.com>
2014-10-27 16:00:58 +0800

    add app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+
+from extension import db
+from web import web
+
+SECRET_KEY = 'TeamBuilding'
+DEBUG=True
+
+SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/TBuilding'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+db.init_app(app)
+
+app.register_blueprint(web, url_prefix='/web')
+
+with app.app_context():
+    db.create_all()
+    print "ok"
+
+if __name__ == '__main__':
+    app.run()
+
